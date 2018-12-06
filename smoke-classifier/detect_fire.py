11a94f2fdc9ce7f8bd1f9f548aa021643523d70c
Kinshuk Govil <kinshuk@google.com>
2018-12-06 15:37:27 -0800

    Remove hardcoded path and add copyright

--- smoke-classifier/detect_fire.py
+++ smoke-classifier/detect_fire.py
@@ -24,7 +24,6 @@ from __future__ import division
 from __future__ import print_function
 
 import sys
-sys.path.insert(0, '/home/fuego/firecam/')
 import settings
 sys.path.insert(0, settings.fuegoRoot + '/lib')
 import collect_args
