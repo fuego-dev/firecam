fa66505ddd80a423c77ae889d8d9f43bd22a5d67
Kinshuk Govil <kinshuk@google.com>
2019-01-29 17:07:58 -0800

    Speedup by reusing graph

--- lib/tf_helper.py
+++ lib/tf_helper.py
@@ -44,35 +44,6 @@ def load_labels(label_file):
         label.append(l.rstrip())
     return label
 
-def read_tensor_from_image_file(file_name,
-                                input_height=299,
-                                input_width=299,
-                                input_mean=0,
-                                input_std=255):
-    input_name = "file_reader"
-    output_name = "normalized"
-    file_reader = tf.read_file(file_name, input_name)
-    if file_name.endswith(".png"):
-        image_reader = tf.image.decode_png(
-            file_reader, channels=3, name="png_reader")
-    elif file_name.endswith(".gif"):
-        image_reader = tf.squeeze(
-            tf.image.decode_gif(file_reader, name="gif_reader"))
-    elif file_name.endswith(".bmp"):
-        image_reader = tf.image.decode_bmp(file_reader, name="bmp_reader")
-    else:
-        image_reader = tf.image.decode_jpeg(
-            file_reader, channels=3, name="jpeg_reader")
-    float_caster = tf.cast(image_reader, tf.float32)
-    dims_expander = tf.expand_dims(float_caster, 0)
-    resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
-    normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
-    sess = tf.Session()
-    result = sess.run(normalized)
-
-    return result
-
-
 def classifySegments(tfSession, graph, labels, segments):
     input_height = 299
     input_width = 299
@@ -96,24 +67,31 @@ def classifySegments(tfSession, graph, labels, segments):
     input_operation = graph.get_operation_by_name(input_name)
     output_operation = graph.get_operation_by_name(output_name)
 
-    for segmentInfo in segments:
-        imgPath = segmentInfo['imgPath']
-        # print(imgPath)
-        t = read_tensor_from_image_file(
-            imgPath,
-            input_height=input_height,
-            input_width=input_width,
-            input_mean=input_mean,
-            input_std=input_std)
+    with tf.Graph().as_default():
+        input_name = "file_reader"
+        output_name = "normalized"
+        file_name_placeholder = tf.placeholder(tf.string, shape=[])
+        file_reader = tf.read_file(file_name_placeholder, input_name)
+        image_reader = tf.image.decode_jpeg(file_reader, channels=3, name="jpeg_reader")
+        float_caster = tf.cast(image_reader, tf.float32)
+        dims_expander = tf.expand_dims(float_caster, 0)
+        resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+        normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
 
-        results = tfSession.run(output_operation.outputs[0], {
-            input_operation.outputs[0]: t
-        })
-        results = np.squeeze(results)
+        with tf.Session() as sess:
+            for segmentInfo in segments:
+                imgPath = segmentInfo['imgPath']
+                # print(imgPath)
+                t = sess.run(normalized, {file_name_placeholder: imgPath})
 
-        top_k = results.argsort()[-5:][::-1]
-        # for i in top_k:
-        #     print(labels[i], results[i])
-        smokeIndex = labels.index('smoke')
-        # print(imgPath, results[smokeIndex])
-        segmentInfo['score'] = results[smokeIndex]
+                results = tfSession.run(output_operation.outputs[0], {
+                    input_operation.outputs[0]: t
+                })
+                results = np.squeeze(results)
+
+                top_k = results.argsort()[-5:][::-1]
+                # for i in top_k:
+                #     print(labels[i], results[i])
+                smokeIndex = labels.index('smoke')
+                # print(imgPath, results[smokeIndex])
+                segmentInfo['score'] = results[smokeIndex]
