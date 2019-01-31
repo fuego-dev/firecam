fa9c9c90a3c793508b82c124f2ec70ab51b0c3ea
Kinshuk Govil <kinshuk@google.com>
2019-01-30 21:21:07 -0800

    gdrive_xfer: add search and download functionality

--- gdrive_xfer.py
+++ gdrive_xfer.py
@@ -30,19 +30,42 @@ import goog_helper
 
 import logging
 
+def searchFiles(service, parentID, minTime=None, maxTime=None, prefix=None):
+    constraints = []
+    if minTime:
+        constraints.append(" modifiedTime > '" + minTime + "' ")
+    if maxTime:
+        constraints.append(" modifiedTime < '" + maxTime + "' ")
+    if prefix:
+        constraints.append(" name contains '" + prefix + "' ")
+    customQuery = ' and '.join(constraints)
+    logging.warn('Query %s', customQuery)
+    items = goog_helper.driveListFilesQuery(service, parentID, customQuery)
+    logging.warn('Found %d files', len(items))
+    print('Files: ', items)
+
+    for item in items:
+        goog_helper.downloadFileByID(service, item['id'], item['name'])
+    return items
+
+
 def main():
     reqArgs = [
         ["d", "dirID", "ID of google drive directory"],
         ["f", "fileName", "fileName of google drive file"],
     ]
     optArgs = [
-        ["u", "upload", "(optional) performs upload vs. download"]
+        ["u", "upload", "(optional) performs upload vs. download"],
+        ["s", "startTime", "(optional) performs search with modifiedTime > startTime"],
+        ["e", "endTime", "(optional) performs search with modifiedTime < endTime"],
     ]
     
     args = collect_args.collectArgs(reqArgs, optionalArgs=optArgs, parentParsers=[goog_helper.getParentParser()])
     googleServices = goog_helper.getGoogleServices(settings, args)
-    if (args.upload):
+    if args.upload:
         goog_helper.uploadFile(googleServices['drive'], args.dirID, args.fileName)
+    elif args.startTime or args.endTime:
+        searchFiles(googleServices['drive'], args.dirID, args.startTime, args.endTime, args.fileName)
     else:
         goog_helper.downloadFile(googleServices['drive'], args.dirID, args.fileName, args.fileName)
 
