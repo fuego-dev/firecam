3647c3659887411147017f3ac4e9137fcb6c81e9
Kinshuk Govil <kinshuk@gmail.com>
2019-05-14 22:30:30 -0700

    dataset: new splits

--- train/dataset_firecam.py
+++ train/dataset_firecam.py
@@ -63,6 +63,7 @@ SPLITS_TO_SIZES = {'train': 109218, 'validation': 12136} # mar 18 diff1
 SPLITS_TO_SIZES = {'train': 109415, 'validation': 12158} # mar 23 diff1
 SPLITS_TO_SIZES = {'train': 114690, 'validation': 12744} # mar 25
 SPLITS_TO_SIZES = {'train': 114195, 'validation': 12689} # apr 16
+SPLITS_TO_SIZES = {'train': 114426, 'validation': 12714} # may 14
 
 _NUM_CLASSES = 2
 
