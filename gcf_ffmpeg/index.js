acd393339075bee5c0ca5887da6a54168fbf9a35
Kinshuk Govil <kinshuk@gmail.com>
2019-07-11 21:36:56 -0700

    gcf_ffmpeg: google drive auth,list,upload
    Code to authenticate, list google drive dir, and upload to google drive
    Still WIP, and not ready for prime time.

--- gcf_ffmpeg/index.js
+++ gcf_ffmpeg/index.js
@@ -20,6 +20,9 @@ const fs = require('fs');
 const path = require('path');
 const request = require('request');
 const rimraf = require('rimraf');
+const google = require('googleapis');
+const gDrive = new google.drive_v3.Drive();
+const gAuthLib = require("google-auth-library");
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const FfmpegCommand = require('fluent-ffmpeg');
 FfmpegCommand.setFfmpegPath(ffmpegPath);
@@ -84,6 +87,160 @@ function getJpegs(mp4File, outFileSpec, cb) {
     cmd.run();
 }
 
+ 
+function gdriveAuthSericeAccountt(keyFile, cb) {
+    function authNow(authClient) {
+        authClient.authorize(function (err, tokens) {
+            if (err) {
+                console.log(err);
+                cb(err);
+                return;
+            } else {
+                console.log("Successfully connected!");
+                cb(null, authClient);
+            }
+        });
+    }
+    const scopes = ['https://www.googleapis.com/auth/drive'];
+    var jwtClient;
+    if (keyFile) {
+        console.log('with key');
+        const keyJson = require(keyFile);
+        jwtClient = new google.google.auth.JWT(keyJson.client_email, null, keyJson.private_key, scopes);
+        authNow(jwtClient);
+    } else {
+        console.log('app default without key');
+        gAuth = new gAuthLib.GoogleAuth({scopes: scopes});
+        gAuth.getApplicationDefault(function (err, jwtClient) {
+            if (err) {
+                console.log(err);
+                cb(err);
+                return;
+            } else {
+                console.log("got ApplicationDefault", jwtClient.projectId);
+                authNow(jwtClient);
+            }
+        });
+    }
+}
+
+function gdriveAuthToken(credsPath, tokenPath, cb) {
+    credsJ = JSON.parse(fs.readFileSync(credsPath));
+    tokenJ = JSON.parse(fs.readFileSync(tokenPath));
+    oauth2 = new google.google.auth.OAuth2(credsJ.installed.client_id, credsJ.installed.client_secret, credsJ.installed.redirect_uris[0]);
+    oauth2.setCredentials({refresh_token: tokenJ.refresh_token});
+    cb(null, oauth2);
+}
+
+function gdriveList(authClient, parentDir) {
+    gDrive.files.list({
+        auth: authClient,
+        supportsTeamDrives: true,
+        includeTeamDriveItems: true,
+        q: '"' + parentDir + '" in parents and trashed = False',
+    }, function (err, response) {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            return;
+        }
+        var files = response.data.files;
+        if (files.length == 0) {
+            console.log('No files found.');
+        } else {
+            console.log('Files from Google Drive:');
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+                console.log('%s (%s)', file.name, file.id);
+            }
+        }
+    });
+}
+function resolveAfter2Seconds(x) { 
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(x);
+      }, 2000);
+    });
+  }
+  
+  function gdriveUploadCB(authClient, filePath, parentDir) {
+    gDrive.files.create({
+        auth: authClient,
+        resource: {
+            'name': path.basename(filePath),
+            'parents': [parentDir]
+        },
+        media: {
+            mimeType: 'image/jpeg',
+            body: fs.createReadStream(filePath)    
+        },
+        supportsTeamDrives: true,
+        fields: 'id'
+    }, function (err, file) {
+        if (err) {
+            // Handle error
+            console.error('Upload error', err);
+        } else {
+            console.log('File: ', file.status, file.statusText, file.data);
+        }
+    });
+}
+
+function gdriveUploadPromise(authClient, filePath, parentDir) {
+    return new Promise((resolve,reject) => {
+        gDrive.files.create({
+            auth: authClient,
+            resource: {
+                'name': path.basename(filePath),
+                'parents': [parentDir]
+            },
+            media: {
+                mimeType: 'image/jpeg',
+                body: fs.createReadStream(filePath)    
+            },
+            supportsTeamDrives: true,
+            fields: 'id'
+        }, function (err, file) {
+            if (err) {
+                // Handle error
+                console.error('Upload error', err);
+                reject(err);
+            } else {
+                console.log('File: ', file.status, file.statusText, file.data);
+                resolve(file);
+            }
+        });
+    });
+}
+
+async function gdriveUploadAsync(authClient, filePath, parentDir) {
+    try {
+        var file = await gdriveUploadPromise(authClient, filePath, parentDir);
+        console.log('await file', file.status, file.statusText, file.data);
+    } catch (err) {
+        console.log('await err', err);
+    }
+}
+  
+
+async function uploadFiles(fromDir, uploadDir, cb) {
+    fileNames = fs.readdirSync(fromDir);
+    files = [];
+    for (var i = 0; i < fileNames.length; i++) {
+        filePath = path.join(dir, fileNames[i]);
+        try {
+            xxxAuthClient();
+            var file = await gdriveUploadPromise(authClient, filePath, uploadDir);
+            console.log('await file', file.status, file.statusText, file.data);
+            files.push(file);
+        } catch (err) {
+            console.log('await err', err);
+            cb(err);
+            break;
+        }
+    }
+    cb(null, files);
+}
 
 /**
  * Responds to any HTTP request.
@@ -101,32 +258,79 @@ exports.helloWorld = (req, res) => {
     console.log('File: ', mp4File);
     downloadMp4(hpwrenUrl, mp4File, function(err, resp, body) {
         if (err) {
+            res.status(400).send('Could not download mp4');
             return;
         }
         console.log('Listing files after download');
         listFiles(tmpDir);
         const outFileSpec = path.join(tmpDir, 'img-%03d.jpg');
         getJpegs(mp4File, outFileSpec, function (err, cmdLine) {
+            if (err) {
+                res.status(400).send('Could not decode mp4');
+                return;
+            }
             console.log('Listing files after ffmpeg');
             listFiles(tmpDir);
+            uploadFiles(tmpDir, req.body.uploadDir, function(err, files) {
+                if (err) {
+                    res.status(400).send('Could not upload jpegs');
+                    return;
+                }
+                rimraf.sync(tmpDir);
+                console.log('All done');
+                res.status(200).send('done');
+            });
         });
-        // rimraf.sync(tmpDir);
-        // upload to google drive
-        console.log('done');
-        res.status(200).send('done');
     });
 };
 
-console.log('argv: ', process.argv)
-if ((process.argv.length > 1) && !process.argv[1].includes('functions-framework')) {
+
+function testHandler() {
     exports.helloWorld(
         {query:{}, body:{
             hostName: 'c1',
             cameraID: 'rm-w-mobo-c',
             yearDir: 2017,
             dateDir: 20170613,
-            qName: 'Q3.mp4'
+            qName: 'Q3.mp4',
+            uploadDir: '1KCdRENKi_b9HgiZ9nzq05P5rTuRH71q2',
         }},
         {status: ()=>({send: (m)=>{console.log('msg', m)}})}
-    )
+    );
+}
+
+
+console.log('argv: ', process.argv)
+if ((process.argv.length > 1) && !process.argv[1].includes('functions-framework')) {
+    const testDir = '1KCdRENKi_b9HgiZ9nzq05P5rTuRH71q2';
+    const TOKEN_PATH = '../../keys/token.json';
+    const CREDS_PATH = '../../keys/credentials.json';
+    const KEY_FILE = '../../keys/service-account.json';
+    // testHandler();
+    // gdriveAuthToken(CREDS_PATH, TOKEN_PATH, function(err, authClient) {
+    gdriveAuthSericeAccountt(KEY_FILE, function(err, authClient) {
+    // gdriveAuthSericeAccountt(null, function(err, authClient) {
+        if (err) {
+            return;
+        }
+        gdriveList(authClient, testDir);
+        // gdriveUploadCB(authClient,
+        //     'index.js',
+        //     testDir
+        // );
+
+        // gdriveUploadPromise(authClient,
+        //     'index.js',
+        //     testDir
+        // ).then(file => {
+        //     console.log('prom res', file.status, file.statusText, file.data);
+        // }).catch(err => {
+        //     console.log('prom rej', err);
+        // });
+
+        // gdriveUploadAsync(authClient,
+        //     'index.js',
+        //     testDir
+        // );
+    });
 }
