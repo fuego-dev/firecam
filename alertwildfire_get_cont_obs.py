f47694d1d3508f8abb9828a9e0793247e0f0dd4b
Morgan-W <52582357+Morgan-W@users.noreply.github.com>
2019-08-22 11:34:08 -0700

    alertwildfire continual observation request (#8)
    * alertwildfire continual observation request
    
    * alertwildfire change to naming convention in api
    
    * recover alertwildfire get cont. observations processing time

---
+++ alertwildfire_get_cont_obs.py
@@ -0,0 +1,58 @@
+# Copyright 2018 The Fuego Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import os
+import sys
+fuegoRoot = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(fuegoRoot, 'lib'))
+sys.path.insert(0, fuegoRoot)
+import settings
+settings.fuegoRoot = fuegoRoot
+import alertwildfire_API
+import collect_args
+import time
+
+def main():
+    reqArgs = [
+        ["c", "cameraID", "ID (code name) of camera"]
+    ]
+    optArgs = [
+        ["i", "interval", "minutes between observations"],
+        ["d", "duration", "duration of observation (minutes)"],
+        ["o", "outputDir", "directory to save the output image"]
+    ]
+    args = collect_args.collectArgs(reqArgs, optionalArgs=optArgs)
+    if not args.outputDir:
+        args.outputDir = settings.downloadDir
+    if not args.duration:
+        args.duration = 30
+    if not args.interval:
+        args.interval = 1
+
+    list_of_downloaded_img_paths = []
+    start_time= time.time()
+    end_time = start_time+float(args.duration)*60
+    next_interval = start_time
+    while True:
+        path = alertwildfire_API.request_current_image(args.outputDir, args.cameraID)
+        list_of_downloaded_img_paths.append(path)
+        next_interval +=float(args.interval)*60
+        if next_interval> end_time:
+            return list_of_downloaded_img_paths
+        time.sleep(int(next_interval-time.time()))
+
+
+if __name__=="__main__":
+    main()
