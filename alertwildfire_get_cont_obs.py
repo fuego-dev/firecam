fb5cbdab7593b21adf5730d95b8783152afe966c
Morgan-W <mwelch@lbl.gov>
2019-09-11 12:01:57 -0700

    documented alertwildfire_get_cont_obs.py

--- alertwildfire_get_cont_obs.py
+++ alertwildfire_get_cont_obs.py
@@ -25,6 +25,15 @@ import collect_args
 import time
 
 def main():
+    """requests the continual recording of a a particular alertwildfire camera and saves images to outputDir
+    Args:
+        -c  cameraID (str): time in hours to store data
+        -i  interval (str): minutes between observations
+        -d  duration (str): duration of observation (minutes)
+        -o  outputDir (str): directory to save the output image
+    Returns:
+        None
+    """
     reqArgs = [
         ["c", "cameraID", "ID (code name) of camera"]
     ]
